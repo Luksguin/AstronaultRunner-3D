e52bdbb63aeaf946e8e2fbabe694cc7df823654c
Lucas Vieira de Albuquerque <guinho.lucas7@gmail.com.br>
2022-10-05 16:51:47 -0300

    add touch controller/ download android platform

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    public float velocity;
+    private Vector2 _pastPosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Move(Input.mousePosition.x - _pastPosition.x);
+        }
+
+        _pastPosition = Input.mousePosition;
+    }
+
+    public void Move(float speed)
+    {
+        transform.position += Vector3.right * Time.deltaTime * speed * velocity;
+    }
+}
