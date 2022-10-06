40c799e6e0a7b1920c382874ccfb7b8dbdb89d76
Lucas Vieira de Albuquerque <guinho.lucas7@gmail.com.br>
2022-10-06 20:32:00 -0300

    update playerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float velocity;
+    public string tagToCheck;
+    private bool _alive;
+    private Vector3 _pos;
 
     [Header("Lerp")]
     public Transform lerper;
     public float lerpTime;
 
+    private void Start()
+    {
+        _alive = true;
+    }
+
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, lerper.position, lerpTime * Time.deltaTime);
+        if (_alive != true) return;
+
+        _pos = lerper.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpTime * Time.deltaTime);
         transform.Translate(transform.forward * velocity * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == tagToCheck)
+        {
+            _alive = false;
+        }
     }
 }
